f692c245913267a27f3da21fd87aa4a04fe9fb4d
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-02 19:03:10 +0530

    Dice Rolling Number

--- src/com/snkLadder/SnakeAndLadder.java
+++ src/com/snkLadder/SnakeAndLadder.java
@@ -3,22 +3,24 @@ package com.snkLadder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int position = 0;
+        int diceRollCount = 0;
         System.out.println("Welcome into Snake and Ladder program !! ");
         System.out.println("position is " + position);
         int diceValue = (int) (Math.random() * 10) % 6 + 1;
         Utility utility = new Utility();
-        utility.playOption(position, diceValue);
+        utility.playOption(position, diceValue, diceRollCount);
 
     }
 }
 
 class Utility {
 
-    public void playOption(int position, int diceValue) {
+    public void playOption(int position, int diceValue, int diceRollCount) {
 
         while (position <= 99) {
 
             int optionValue = (int) (Math.random() * 10) % 3 + 1;
+            diceRollCount++;
 
             switch (optionValue) {
                 case 1:
@@ -44,6 +46,6 @@ class Utility {
                     break;
             }
         }
+        System.out.println("dice play number:: " + diceRollCount);
     }
-
 }
