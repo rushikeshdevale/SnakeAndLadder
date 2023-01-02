6ddee904ffb8132e04bc4275d9194069ac1e6405
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-02 18:44:07 +0530

    Get your Position

--- src/com/snkLadder/SnakeAndLadder.java
+++ src/com/snkLadder/SnakeAndLadder.java
@@ -1,15 +1,33 @@
 package com.snkLadder;
 
 public class SnakeAndLadder {
-
     public static void main(String[] args) {
-        int Position = 0;
+        int position = 0;
         System.out.println("Welcome into Snake and Ladder program !!");
-        System.out.println("Your Starting Position is : " + Position);
-        Dice();
-    }
-    public static void Dice(){
-        int Dice = (int)Math.floor(Math.random()*10)%6+1;
-        System.out.println("Your Dice Value is : " +Dice);
+        System.out.println("Your Starting Position is : " + position);
+        int die = (int) (Math.random() * 10) % 6 + 1;
+        System.out.println("dice value is " + die);
+        int value = (int) (Math.random() * 10) % 3 + 1;
+        System.out.println("case number is " + value);
+
+        switch(value) {
+            case 1:
+                System.out.println( "No Play You are in the same position." );
+                break;
+
+            case 2:
+                position = (position + die);
+                System.out.println( "your position after ladder is ::" + position );
+                break;
+
+            case 3:
+                position = (position - die);
+
+                if(position<0){
+                    position = 0;
+                }
+                System.out.println( "Your Position after snake is::" + position );
+                break;
+        }
     }
 }
