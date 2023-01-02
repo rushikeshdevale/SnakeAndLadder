a2e2117a016d3e0120e68a0defa0999d278cef67
Rushikesh <rushikeshdevale2658@gmail.com>
2023-01-02 14:26:24 +0530

    Welcome

---
+++ src/com/snkLadder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.snkLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int Position = 0;
+        System.out.println("Welcome into Snake and Ladder program !!");
+        System.out.println("Your Starting Position is : " + Position);
+    }
+
+}
